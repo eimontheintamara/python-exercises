1282dc265828af9f5291ca6c47b3b4f61a68b241
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-06 16:01:37 +0530

    tfour

--- ex24.py
+++ ex24.py
@@ -1,38 +1,32 @@
 print("Let's practice everything.")
-print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs')
+print('You\'d need to know \'bout escapes with \\ that do:')
+print('\n newlines and \t tabs.')
 
 poem="""
-\t The lovely world
+\tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
-nor comprehend passion from intuition
+nor comprehend passion for intuition
 and requires an explanation
 \n\t\twhere there is none.
-"""
-
-print("-----------------------------------"
 print(poem)
-print("-----------------------------------"
+
+
+
 
 
 five=10-2+3-6
-print("Htis should be five:%s" %five)
+print(f"This should be five:{five}")
+
 
 def secret_formula(started):
-    jelly_besans=started * 5000
-    jars=jelly_beans/1000
-    crates=jars/100
+    jelly_beans=started * 500
+    jars=jelly_beans / 1000
+    crates=jars / 100
     return jelly_beans,jars,crates
 
 
-    start_point=10000
-    beans,jars,crates=secret_formula(start_point)
+start_point=10000
+beans,jars,crates=secret_formula(start_point)
 
-    print("With a starting point of: %d" %start_point
-    print("We'd have %d beans,%d jars,and %d crates." % (beans,jars,crates))
-
-
-    start_point=start_port/10
-
-    print("We can also do that this way:")
-    print("We'd have %d beans,%d jars,and %d crates." % secret_formula(start_point))
+#remember that this is another way to format a string

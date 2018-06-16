9d04bccfd374f93bd5c3ee69f1b5e2a47a7a64a5
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-16 19:24:27 +0530

    refifty

--- ex50.py
+++ ex50.py
@@ -1,8 +1,7 @@
-from flask import Flask,render_template
-app=Flask(__name__,template_folder='template')
+from flask import Flask
+app=Flask(__name__)
 @app.route('/')
-def hello():
-    greeting="Hello** Ei Mon Theint**Warmly welcome**"
-    return render_template("index.html",greeting=greeting)
+def hello(): 
+    return "Hello **Ei Mon Theint** welcome"
 if __name__=="__main__":
     app.run()

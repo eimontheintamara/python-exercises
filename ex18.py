9b224ec1135deeb10499008ce3cbf29607bac726
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-27 15:49:44 +0630

    Names,Variables,Code,Functions

--- ex18.py
+++ ex18.py
@@ -4,7 +4,7 @@ def print_two(*args):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
 #ok,that *args is actually pointless,we can just do this
-def print_two_again(arg1,arg2):
+def print_two_again(arg1, arg2):
     print(f"arg1: {arg1}, arg2: {arg2}")
 
 #this just takes one argument
@@ -15,10 +15,8 @@ def print_one(arg1):
 def print_none():
     print("I got nothin'.")
 
-
-
-print_two("Zed","Shaw")
-print_two_again("Xed","Shaw")
+print_two("Zed", "Shaw")
+print_two_again("Xed", "Shaw")
 print_one("First!")
 print_none()
 

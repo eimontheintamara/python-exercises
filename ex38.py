cec05db7b08d34743b0803f420ac4e3b87a3823b
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-27 15:04:59 +0630

    Doing Things to Lists

--- ex38.py
+++ ex38.py
@@ -1,17 +1,17 @@
-ten_things="Apples Oranges crows Telephone Light Sugar"
+ten_things = "Apples Oranges crows Telephone Light Sugar"
 
 print("Wait there are not 10 things in that list.Let's fix")
 
-stuff=ten_things.split(' ')
-more_stuff=["Day","Night","Song","Frisbee","Corn","Banana","Gitl","Boy"]
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Gitl", "Boy"]
 
-while len(stuff) !=10:
-    next_one=more_stuff.pop()
-    print("Adding:",next_one)
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding:", next_one)
     stuff.append(next_one)
     print(f"There are {len(stuff)} items nows.")
     
-print("There w go:",stuff)
+print("There w go:", stuff)
 
 print("Let's go do some things with stuff.")
 

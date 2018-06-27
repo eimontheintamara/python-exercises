701e282709b8a4ec61c05c88eb7df316dbc2fd07
eimontheintamara <eimontheintamarapura@gmail.com>
2018-06-27 15:59:01 +0630

    Printing,Printing,Printing

--- ex9.py
+++ ex9.py
@@ -1,8 +1,8 @@
 #Here's some new strange stuff,remember type it exactly.
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-print("Here are the days: ",days)
-print("Here are the months:",months)
+print("Here are the days: ", days)
+print("Here are the months:", months)
 print("""
         There's something going on here.
         With the three double-quotes.
